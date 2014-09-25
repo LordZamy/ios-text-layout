73becc63ba3c3b5f0865c1cd5420e3718f981248
Samarth Wahal <samarthwahal@hotmail.com>
2014-09-24 19:01:38 -0500

    basic draw background on canvas

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-	
+	var canvas = document.getElementById('render');
+	var ctx = canvas.getContext('2d');
+
+	addImage('images/ios-background.png');
+
+	function addImage(url) {
+		var img = new Image();
+		img.src = url;
+
+		img.onload = function() {
+			ctx.drawImage(img, 0, 0, img.width / 2, img.height / 2);
+		}
+		console.log(img);
+	}
 });
