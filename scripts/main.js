e0e80c1cc65a4d92669416d9e927487ab5411a63
Samarth Wahal <samarthwahal@hotmail.com>
2014-09-25 11:21:38 -0500

    add roundedImage() and tweak addImage() to accept x, y, scaleX, scaleY params

--- scripts/main.js
+++ scripts/main.js
@@ -4,13 +4,26 @@ $(document).ready(function() {
 
 	addImage('images/ios-background.png');
 
-	function addImage(url) {
+	function addImage(url, x, y, scaleX, scaleY) {
 		var img = new Image();
 		img.src = url;
 
 		img.onload = function() {
-			ctx.drawImage(img, 0, 0, img.width / 2, img.height / 2);
+			ctx.drawImage(img, x || 0, y || 0, img.width * (scaleX || 0.5), img.height * (scaleY || 0.5));
 		}
-		console.log(img);
+	}
+
+	function roundedImage(x, y, width, height, radius) {
+		ctx.beginPath();
+		ctx.moveTo(x + radius, y);
+		ctx.lineTo(x + width - radius, y);
+		ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+		ctx.lineTo(x + width, y + height - radius);
+		ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+		ctx.lineTo(x + radius, y + height);
+		ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+		ctx.lineTo(x, y + radius);
+		ctx.quadraticCurveTo(x, y, x + radius, y);
+		ctx.closePath();
 	}
 });
